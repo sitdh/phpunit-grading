c57c46bbdb29c84867008bd80da45532eb87284f
Sitdhibong Laokok <sitdhibong@gmail.com>
2017-10-16 19:58:45 +0700

    Update SimpleGrading.php

--- src/SimpleGrading.php
+++ src/SimpleGrading.php
@@ -4,18 +4,18 @@ class SimpleGrading {
 
     public function evaluate($studentId, $score) {
 
-        $gradeLetter = 'F';
+        $gradeLetter = '';
         
-        if ($score >= 80) {
+        if ($score >= 81) {
             $gradeLetter = 'A';
-        } elseif ($score >= 80) {
+        } elseif ($score > 71) {
+            $gradeLetter = 'B';
+        } elseif ($Score >= 61) {
             $gradeLetter = 'C';
-        } elseif ($score >= 50) {
+        } elseif ($score >= 51) {
             $gradeLetter = 'D';
-        } elseif ($Score >= 60) {
-            $gradeLetter = 'Q';
-        } elseif ($score >= 70) {
-            $gradeLetter = 'A';
+        } else {
+            $gradeLetter = 'F';
         }
 
         return "1234567890 => '{$gradeLetter}'";
